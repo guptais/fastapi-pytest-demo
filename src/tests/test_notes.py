95df3787512b699c2498eb83660c75fc617a4cb4
Isha Gupta <15913179+guptaisha5484@users.noreply.github.com>
2021-02-13 20:04:20 +1100

    Updated validation for put and delete

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -97,7 +97,10 @@ def test_update_note(test_app, monkeypatch):
     [
         [1, {}, 422],
         [1, {"description": "bar"}, 422],
-        [999, {"title": "foo", "description": "bar"}, 404]
+        [999, {"title": "foo", "description": "bar"}, 404],
+        [1, {"title": "1", "description": "bar"}, 422],
+        [1, {"title": "foo", "description": "1"}, 422],
+        [0, {"title": "foo", "description": "bar"}, 422]
     ],
 )
 def test_update_note_valid(test_app, monkeypatch, id, payload, status_code):
@@ -128,7 +131,7 @@ def test_remove_note(test_app, monkeypatch):
     assert response.json() == test_data
 
 
-def test_remove_note_invalid_id(test_app, monkeypatch):
+def test_remove_note_incorrect_id(test_app, monkeypatch):
     async def mock_get(id):
         return None
 
@@ -137,3 +140,6 @@ def test_remove_note_invalid_id(test_app, monkeypatch):
     response = test_app.delete("/notes/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Note not found"
+
+    response = test_app.delete("/notes/0/")
+    assert response.status_code == 422
